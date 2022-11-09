366737bfd0034bf1aa6e2bd5bee154d564539b5f
Wilson Júnior <wilsonpjunior@gmail.com>
2022-11-09 15:40:20 -0300

    Add test about empty reverse proxy destinations

--- controllers/rpaas_instance_controller_test.go
+++ controllers/rpaas_instance_controller_test.go
@@ -7,11 +7,96 @@ import (
 	v1alpha1 "github.com/tsuru/acl-operator/api/v1alpha1"
 	rpaasv1alpha1 "github.com/tsuru/rpaas-operator/api/v1alpha1"
 
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	controllerruntime "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
+
+func (suite *ControllerSuite) TestRPaaSInstanceReconcilerEmptyReconcile() {
+	ctx := context.Background()
+	rpaasInstance := &rpaasv1alpha1.RpaasInstance{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-rpaas",
+			Namespace: "default",
+			Labels: map[string]string{
+				"rpaas.extensions.tsuru.io/instance-name": "my-rpaas",
+				"rpaas.extensions.tsuru.io/service-name":  "my-service",
+			},
+		},
+		Spec: rpaasv1alpha1.RpaasInstanceSpec{
+			Binds:            []rpaasv1alpha1.Bind{},
+			AllowedUpstreams: []rpaasv1alpha1.AllowedUpstream{},
+		},
+	}
+
+	reconciler := &RpaasInstanceReconciler{
+		Client: fake.NewClientBuilder().WithScheme(scheme.Scheme).WithRuntimeObjects(rpaasInstance).Build(),
+		Scheme: scheme.Scheme,
+	}
+	_, err := reconciler.Reconcile(ctx, controllerruntime.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      rpaasInstance.Name,
+			Namespace: rpaasInstance.Namespace,
+		},
+	})
+	suite.Require().NoError(err)
+
+	existingACL := &v1alpha1.ACL{}
+	err = reconciler.Client.Get(ctx, types.NamespacedName{
+		Name:      rpaasInstance.Name,
+		Namespace: rpaasInstance.Namespace,
+	}, existingACL)
+	suite.Require().True(k8sErrors.IsNotFound(err))
+}
+
+func (suite *ControllerSuite) TestRPaaSInstanceReconcilerDelete() {
+	ctx := context.Background()
+	rpaasInstance := &rpaasv1alpha1.RpaasInstance{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-rpaas",
+			Namespace: "default",
+			Labels: map[string]string{
+				"rpaas.extensions.tsuru.io/instance-name": "my-rpaas",
+				"rpaas.extensions.tsuru.io/service-name":  "my-service",
+			},
+		},
+		Spec: rpaasv1alpha1.RpaasInstanceSpec{
+			Binds:            []rpaasv1alpha1.Bind{},
+			AllowedUpstreams: []rpaasv1alpha1.AllowedUpstream{},
+		},
+	}
+
+	acl := &v1alpha1.ACL{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: rpaasInstance.Namespace,
+			Name:      rpaasInstance.Name,
+		},
+		Spec: v1alpha1.ACLSpec{
+			Destinations: []v1alpha1.ACLSpecDestination{},
+		},
+	}
+
+	reconciler := &RpaasInstanceReconciler{
+		Client: fake.NewClientBuilder().WithScheme(scheme.Scheme).WithRuntimeObjects(rpaasInstance, acl).Build(),
+		Scheme: scheme.Scheme,
+	}
+	_, err := reconciler.Reconcile(ctx, controllerruntime.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      rpaasInstance.Name,
+			Namespace: rpaasInstance.Namespace,
+		},
+	})
+	suite.Require().NoError(err)
+
+	existingACL := &v1alpha1.ACL{}
+	err = reconciler.Client.Get(ctx, types.NamespacedName{
+		Name:      rpaasInstance.Name,
+		Namespace: rpaasInstance.Namespace,
+	}, existingACL)
+	suite.Require().True(k8sErrors.IsNotFound(err))
+}
 
 func (suite *ControllerSuite) TestRPaaSInstanceReconcilerSimpleReconcile() {
 	ctx := context.Background()
